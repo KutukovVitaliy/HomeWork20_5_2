1338d8ead8f0e64f249892988ed314040164f78b
kvitadmin <kutukov@list.ru>
2021-08-14 09:55:34 +0300

    Last commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <ctime>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ofstream TxtFile("../pic.txt");
+    int Length, Width;
+    std::srand(std::time(nullptr));
+    std::cout << "Input the width: ";
+    std::cin >> Width;
+    std::cout << "Input the length: ";
+    std::cin >> Length;
+    for(int i=0; i<Length; i++){
+        for(int j=0; j<Width; j++){
+            TxtFile << std::rand() % 2;
+        }
+        TxtFile << std::endl;
+    }
+    TxtFile.close();
     return 0;
 }
